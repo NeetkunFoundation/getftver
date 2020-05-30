8f4c3ee634bce575ceae5fa4ed3506e5c3c141d2
azure <rz7@outlook.jp>
2020-05-31 02:20:40 +0900

    small fix

--- getftver.c
+++ getftver.c
@@ -1,22 +1,19 @@
 #include <Windows.h>
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <varargs.h>
 
 #include <tchar.h>
-
 
 /* standard type definitions */
 typedef void *FT_Library;
 typedef signed int FT_Int;
 typedef int FT_Error;
 
-
 /* library procedures */
 typedef FT_Error (*PROC_FT_Init_FreeType)(FT_Library *);
 typedef FT_Error (*PROC_FT_Done_FreeType)(FT_Library);
 typedef void (*PROC_FT_Library_Version)(FT_Library, FT_Int *, FT_Int *, FT_Int *);
-
 
 /* macros for FT_Error */
 #define FT_FAILURE(E) ((E) != 0)
@@ -30,8 +27,8 @@ static PROC_FT_Library_Version FT_Library_Version;
 
 static HMODULE hLibrary;
 
-
-static bool create_libraries(void);
+static BOOL PrintError(DWORD, LPCTSTR);
+static BOOL InitLibrary(VOID);
 
 int main(void)
 {
@@ -39,11 +36,13 @@ int main(void)
     FT_Int major, minor, patch;
     FT_Error error;
 
-    if (!init_libraries()) {
+    if (!InitLibrary())
+    {
         return EXIT_FAILURE;
     }
 
-    if (FT_FAILURE(error = FT_Init_FreeType(&library))) {
+    if (FT_FAILURE(error = FT_Init_FreeType(&library)))
+    {
         _tprintf(_T("Error '0x%x' has been occurred while INITIALIZING FreeType library.\n"), error);
         return EXIT_FAILURE;
     }
@@ -52,7 +51,8 @@ int main(void)
 
     _tprintf(_T("FreeType version %d.%d.%d"), major, minor, patch);
 
-    if (FT_FAILURE(error = FT_Done_FreeType(library))) {
+    if (FT_FAILURE(error = FT_Done_FreeType(library)))
+    {
         _tprintf(_T("Error '0x%x' has been occurred while DESTROYING FreeType library.\n"), error);
         return EXIT_FAILURE;
     }
@@ -61,33 +61,68 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
-static bool init_libraries(void)
+static BOOL PrintError(DWORD dwError, LPCTSTR lpDetails, size_t szDetailsLength)
 {
-    DWORD numCharsWritten;
+    DWORD dwWritten;
     TCHAR lpBuffer[512];
+    dwWritten = FormatMessage(
+        FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL,
+        dwError,
+        0,
+        lpBuffer,
+        sizeof(lpBuffer) / sizeof(TCHAR),
+        NULL);
+
+    if (!dwWritten)
+    {
+        _tprintf(_T("FormatMessage is exited with error code 0x%x\n"), GetLastError());
+        return FALSE;
+    }
+
+    _tprintf_s(_T("%s%s"), lpDetails, szDetailsLength, lpBuffer, dwWritten);
+    return FALSE;
+}
+
+static BOOL init_libraries(void)
+{
+    static LPCSTR lpProcNames[3] = {
+        "FT_Init_FreeType",
+        "FT_Done_FreeType",
+        "FT_Library_Version",
+    };
+    LPCSTR lpName;
+    FARPROC lpProc;
+    SIZE_T i;
 
     hLibrary = LoadLibrary(LIBRARY_NAME);
     if (!hLibrary)
     {
-        numCharsWritten = FormatMessage(
-            FORMAT_MESSAGE_FROM_SYSTEM,
-            NULL,
-            GetLastError(),
-            0,
-            lpBuffer,
-            sizeof(lpBuffer) / sizeof(TCHAR),
-            NULL);
-        if (!numCharsWritten)
-        {
-            _tprintf(_T("FormatMessage is exited with error code 0x%x\n"), GetLastError());
-            return false;
-        }
-        _tprintf_s(_T("Could not load library '") LIBRARY_NAME _T("': %s\n"), lpBuffer, numCharsWritten);
+        return PrintError(GetLastError(), _T("Could not load library '") LIBRARY_NAME _T("': "));
     }
 
-    FT_Init_FreeType = (PROC_FT_Init_FreeType)GetProcAddress(hLibrary, "FT_Init_FreeType");
-    FT_Done_FreeType = (PROC_FT_Done_FreeType)GetProcAddress(hLibrary, "FT_Done_FreeType");
-    FT_Library_Version = (PROC_FT_Library_Version)GetProcAddress(hLibrary, "FT_Library_Version");
+    for (i = 0; i < 3; ++i)
+    {
+        FARPROC lpProc = GetProcAddress(hLibrary, lpName);
+        if (!lpProc)
+        {
+            TCHAR lpDetails[256];
+            _stprintf_s(lpDetails, 255, _T("Function '%s' is not available: "), lpName, strlen(lpName));
+            return PrintError(GetLastError(), lpDetails, _tcslen(lpDetails));
+        }
+        switch (i)
+        {
+        case 0:
+            FT_Init_FreeType = lpProc;
+            break;
+        case 1:
+            FT_Done_FreeType = lpProc;
+            break;
+        case 2:
+            FT_Library_Version = lpProc;
+            break;
+        }
+    }
 
-    return true;
+    return TRUE;
 }
